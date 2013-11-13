5e89d59e721ce7c6d0cf8913e7038019bbba6d5d
Juraj Huska <gooseka2@gmail.com>
2013-11-13 23:18:12 +0100

    adding interfaces for first iteration

---
+++ AutomaticPsychiatrist.java
@@ -0,0 +1,16 @@
+ 
+
+/**
+ * Represents a new way of curing mental illnesses.
+ * 
+ * @author <a href="359345@mail.muni.cz">Juraj Huska</a>
+ */
+public interface AutomaticPsychiatrist
+{
+    /**
+     * Method will wait for some time. During waiting, it prints on the standard output string - "hmmm, hmmm, interesting."
+     * 
+     * The waiting period is determined by the constructor parameter <code>long thinkingTimeout</code> in miliseconds.
+     */
+    void thinkSomeTime();
+}
---
+++ TextAnalyzer.java
@@ -0,0 +1,31 @@
+ 
+
+
+/**
+ * Class for giving the best answer for a question.
+ * 
+ * @author <a href="359345@mail.muni.cz">Juraj Huska</a>
+ */
+public interface TextAnalyzer
+{
+   /**
+    * Gets the best answer according to the given input.
+    * 
+    * It will choose the answer from the given param <code>possibleInputsAndAnswers</code>.
+    * Each entry in that array is in following format: "question - answer".
+    * It should choose the answer for the most similar question.
+    * 
+    * An example call of this method would be:
+    * 
+    * <pre>
+    * String[] somePossibilities = {"How are you ? - Great, thanks mate!", "How you been ? - Fine mate!", "How are you doing ? - I am fine."}
+    * TextAnalyzer analyzer = new TextAnalyzerImpl();
+    * analyzer.getBestAnswer("How are you doing ?", somePossibilities); //would return "I am fine."
+    * </pre>
+    * 
+    * @param input                     the question which was asked
+    * @parem possibleInputsAndAnswers  array with possible questions and answers
+    * @return                          the answer for the most similar question as <code>input</code>. If there is no such question, the string "I am tired, bye for now" is returned.
+    */
+   String getBestAnswer(String input, String[] possibleInputsAndAnswers);
+}
